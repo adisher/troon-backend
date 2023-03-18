a1fa22bf155e07e25aab62bbe11e992aa8c00e83
Adil Sher <adilsher973@gmail.com>
2023-03-18 08:25:57 +0500

    upload docs

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const authMiddleware = require("./middlewares/authMiddleware");
 var app = express();
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, __dirname);
+    cb(null, path.join(__dirname, 'uploads'));
   },
   filename: function (req, file, cb) {
     console.log("f: ", file)
