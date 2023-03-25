03861eed504655bdbc509a8c1599a5001d3f0658
Adil Sher <adilsher973@gmail.com>
2023-03-25 09:25:33 +0500

    cors update

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
-app.use(inputMiddleware.handleOptions);
+// app.use(inputMiddleware.handleOptions);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(__dirname + '/tmp'));
 app.use('/tmp', express.static('tmp'));
